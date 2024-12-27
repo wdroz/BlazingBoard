c5b1bcf2f6726c654fd89170fecf6ffcb87e9908
William Droz <william.droz.ch@gmail.com>
2024-12-27 14:00:30 +0100

    chore: replaced links by text and input

--- blazing_board/src/main.rs
+++ blazing_board/src/main.rs
@@ -23,16 +23,8 @@ pub fn Hero() -> Element {
     rsx! {
         div { id: "hero",
             img { src: HEADER_MAIN, id: "main" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
-                    "💫 VSCode Extension"
-                }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+            label { id: "textToWrite", "Please write this text" }
+            input { id: "textUser" }
         }
     }
 }
