b96251ceb6beaef59aba77a92f9cdf3f8b231ad5
William Droz <william.droz.ch@gmail.com>
2024-12-30 19:19:33 +0100

    feat: green when correct, red when wrong

--- blazing_board/src/main.rs
+++ blazing_board/src/main.rs
@@ -22,20 +22,31 @@ fn App() -> Element {
 pub fn Hero() -> Element {
     let sentence_to_write = "Please write this text";
     let mut current_word_indice = use_signal(|| 0);
-    let mut current_text =use_signal(|| String::new()); 
+    let mut current_text =use_signal(|| String::new());
+    let mut user_words = use_signal(|| Vec::<String>::new());
+
     rsx! {
         div { id: "hero",
             img { src: HEADER_MAIN, id: "main" }
             div { id: "words",
                 for (i , word) in sentence_to_write.split(" ").enumerate() {
                     if i < current_word_indice() {
-                        div { class: "previous", "{word}" }
+                        if user_words().len() - 1 >= i {
+                            if user_words()[i] == word {
+                                div { class: "previous_correct", "{word}" }
+                            } else {
+                                div { class: "previous_wrong", "{word}" }
+                            }
+                        }
                     } else if i == current_word_indice() {
                         div { id: "current", "{word}" }
                     } else {
                         div { "{word}" }
                     }
                 }
+            }
+            for w in user_words() {
+                p { "debug {w}" }
             }
             label { id: "textToWrite", "Please write this text" }
             input {
@@ -45,6 +56,15 @@ pub fn Hero() -> Element {
                     let words: Vec<&str> = data.split(" ").collect();
                     current_word_indice.set((words.len() - 1) + current_word_indice());
                     if let Some(last) = words.last() {
+                        if current_text().len() > last.len() {
+                            let mut newvec = user_words().to_vec();
+                            for w in words.clone() {
+                                if w != " " && w != "" {
+                                    newvec.push(w.to_string());
+                                }
+                            }
+                            user_words.set(newvec);
+                        }
                         current_text.set(last.to_string());
                     }
                 },
