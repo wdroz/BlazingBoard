4b7b1f67da5047f25dd00677ce0192bd28b43fcc
William Droz <william.droz.ch@gmail.com>
2024-12-31 15:28:04 +0100

    chore: removed echoserver from boilerplate

--- blazing_board/src/main.rs
+++ blazing_board/src/main.rs
@@ -14,7 +14,6 @@ fn App() -> Element {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         Hero {}
-        Echo {}
     }
 }
 
@@ -76,38 +75,6 @@ pub fn Hero() -> Element {
             }
         }
     }
-}
-
-/// Echo component that demonstrates fullstack server functions.
-#[component]
-fn Echo() -> Element {
-    let mut response = use_signal(|| String::new());
-
-    rsx! {
-        div { id: "echo",
-            h4 { "ServerFn Echo" }
-            input {
-                placeholder: "Type here to echo...",
-                oninput: move |event| async move {
-                    let data = echo_server(event.value()).await.unwrap();
-                    response.set(data);
-                },
-            }
-
-            if !response().is_empty() {
-                p {
-                    "Server echoed: "
-                    i { "{response}" }
-                }
-            }
-        }
-    }
-}
-
-/// Echo the user input on the server.
-#[server(EchoServer)]
-async fn echo_server(input: String) -> Result<String, ServerFnError> {
-    Ok(input)
 }
 
 #[server(TextServer)]
