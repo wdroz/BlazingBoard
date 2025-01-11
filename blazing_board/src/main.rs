cb8d3e16364818a9af83908cffae723365994388
William Droz <william.droz.ch@gmail.com>
2025-01-11 19:26:34 +0100

    feat: show two lines chunks by chunks

--- blazing_board/src/main.rs
+++ blazing_board/src/main.rs
@@ -39,10 +39,14 @@ fn App() -> Element {
 
 #[component]
 pub fn TypingWords() -> Element {
-    let mut current_word_indice = use_signal(|| 0);
+    let mut current_chunk_index = use_signal(|| 0);
+    let mut current_word_in_chunk_index = use_signal(|| 0);
     let mut current_text = use_signal(|| String::new());
     let mut user_words = use_signal(|| Vec::<String>::new());
     let mut start_typing_at = use_signal(|| None);
+    let mut all_nb_correct = use_signal(|| 0);
+    let mut all_nb_wrong = use_signal(|| 0);
+
     let response_sentence_to_write = use_resource(|| async move {
         get_text()
             .await
@@ -53,14 +57,23 @@ pub fn TypingWords() -> Element {
         .split_whitespace()
         .map(|w| w.to_string())
         .collect::<Vec<String>>();
-    let nb_words_to_write = sentence_to_write_words.len();
+
+    // Group words into chunks of 15
+    let sentence_to_write_chunks = sentence_to_write_words
+        .chunks(15)
+        .map(|chunk| chunk.to_vec())
+        .collect::<Vec<Vec<String>>>();
+
+    let nb_chunks_to_write = sentence_to_write_chunks.len();
     let mut nb_correct = 0;
     let mut nb_wrong = 0;
     let mut accuracy = 0.0;
     let mut nb_seconds = 0;
     let mut wpm = 0.0;
+
     let mut timer_value = use_signal(|| 60);
     let mut time_should_decrement = use_signal(|| false);
+
     let _ = use_coroutine(move |_: UnboundedReceiver<i32>| async move {
         loop {
             sleep(std::time::Duration::from_secs(1)).await;
@@ -70,56 +83,72 @@ pub fn TypingWords() -> Element {
         }
     });
 
+    let current_chunk = {
+        let index = current_chunk_index();
+        if index < nb_chunks_to_write {
+            sentence_to_write_chunks[index].clone()
+        } else {
+            vec![]
+        }
+    };
+
     rsx! {
         div { id: "TypingWords",
             img { src: HEADER_MAIN, id: "main" }
             div { id: "timer", "{timer_value}" }
             div { id: "words",
-                for (i , word) in sentence_to_write_words.into_iter().enumerate() {
-                    if i < current_word_indice() {
-                        if user_words().len() - 1 >= i {
-                            if user_words()[i] == word {
+                for (i , word) in current_chunk.iter().enumerate() {
+                    if i < current_word_in_chunk_index() {
+                        if user_words().len() > i {
+                            if user_words()[i] == *word.clone() {
                                 {
-                                    nb_correct = nb_correct + 1;
+                                    nb_correct += 1;
                                 }
                                 div { class: "previous_correct", "{word}" }
                             } else {
                                 {
-                                    nb_wrong = nb_wrong + 1;
+                                    nb_wrong += 1;
                                 }
                                 div { class: "previous_wrong", "{word}" }
                             }
                         }
-                    } else if i == current_word_indice() {
+                    } else if i == current_word_in_chunk_index() {
                         div { id: "current", "{word}" }
                     } else {
                         div { "{word}" }
                     }
                 }
             }
-            if user_words().len() < nb_words_to_write && timer_value() > 0 {
+            if current_chunk_index() < nb_chunks_to_write && timer_value() > 0 {
                 input {
                     id: "textUser",
                     oninput: move |event| {
+                        let current_chunk_clone = current_chunk.clone();
                         async move {
                             time_should_decrement.set(true);
                             if start_typing_at().is_none() {
                                 start_typing_at.set(Some(get_timestamp_seconds_now()));
                             }
                             let data = event.value();
-                            let words: Vec<&str> = data.split(" ").collect();
-                            current_word_indice.set((words.len() - 1) + current_word_indice());
-                            if let Some(last) = words.last() {
-                                if data.ends_with(" ") {
-                                    let mut newvec = user_words().to_vec();
-                                    for w in words.clone() {
-                                        if w != " " && w != "" {
-                                            newvec.push(w.to_string());
-                                        }
-                                    }
-                                    user_words.set(newvec);
+                            if data.ends_with(" ") {
+                                let mut new_words = user_words().to_vec();
+                                new_words.push(data.trim().to_string());
+                                user_words.set(new_words);
+                                let next_word_index = current_word_in_chunk_index() + 1;
+                                if next_word_index >= current_chunk_clone.len() {
+                                    current_word_in_chunk_index.set(0);
+                                    current_chunk_index.set(current_chunk_index() + 1);
+                                    all_nb_correct.set(all_nb_correct() + nb_correct);
+                                    all_nb_wrong.set(all_nb_wrong() + nb_wrong);
+                                    nb_correct = 0;
+                                    nb_wrong = 0;
+                                    user_words.set(vec![]);
+                                } else {
+                                    current_word_in_chunk_index.set(next_word_index);
                                 }
-                                current_text.set(last.to_string());
+                                current_text.set(String::new());
+                            } else {
+                                current_text.set(data.clone());
                             }
                         }
                     },
@@ -127,21 +156,28 @@ pub fn TypingWords() -> Element {
                 }
             } else {
                 {
+                    all_nb_correct();
+                    all_nb_wrong();
                     time_should_decrement.set(false);
-                    accuracy = f64::from(nb_correct) / f64::from(nb_correct + nb_wrong);
+                    accuracy = f64::from(all_nb_correct() + nb_correct)
+                        / f64::from(all_nb_correct() + nb_correct + all_nb_wrong() + nb_wrong);
                     if let Some(start_typing_at_some) = start_typing_at() {
                         nb_seconds = get_timestamp_seconds_now() - start_typing_at_some;
                         let nb_minutes = nb_seconds as f64 / 60.0;
-                        wpm = f64::from(nb_correct) / nb_minutes;
+                        wpm = f64::from(all_nb_correct() + nb_correct) / nb_minutes;
                     }
                 }
-                div { "Accuracy:  {nb_correct} / {nb_correct + nb_wrong} = {accuracy}" }
+                div {
+                    "Accuracy:  {all_nb_correct() + nb_correct} / {all_nb_correct() + nb_correct + all_nb_wrong() + nb_wrong} = {accuracy}"
+                }
                 div { "time(s):  {nb_seconds}" }
                 div { "wpm:  {wpm}" }
             }
         }
     }
 }
+
+
 
 #[server(TextServer)]
 async fn get_text() -> Result<String, ServerFnError> {
