e02e190b3f23b35b09a77be1cc3938a022aefb98
William Droz <william.droz.ch@gmail.com>
2025-01-25 19:43:52 +0100

    feat: get all comments of the most popular story of the last 24h

--- content_updater/src/main.rs
+++ content_updater/src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-
+use std::{thread, time};
 use headless_chrome::{Browser, LaunchOptions};
 
-fn query(input: &str) -> Result<()> {
+fn query() -> Result<()> {
     let browser = Browser::new(
         LaunchOptions::default_builder()
             .sandbox(false)
@@ -10,21 +10,37 @@ fn query(input: &str) -> Result<()> {
             .expect("Could not find chrome-executable"),
     )?;
     let tab = browser.new_tab()?;
-    tab.navigate_to("https://en.wikipedia.org")?
-        .wait_for_element("input#searchInput")?
-        .click()?;
-    tab.type_str(input)?.press_key("Enter")?;
-    match tab.wait_for_element("div.shortdescription") {
-        Err(e) => eprintln!("Query failed: {e:?}"),
-        Ok(e) => match e.get_description()?.find(|n| n.node_name == "#text") {
-            Some(n) => println!("Result for `{}`: {}", &input, n.node_value),
-            None => eprintln!("No shortdescription-node found on page"),
-        },
+
+    // Navigate to the page
+    tab.navigate_to("https://hn.algolia.com/?dateRange=last24h&page=0&prefix=false&query=&sort=byPopularity&type=story")?;
+
+    // Allow some time for the page to load
+    thread::sleep(time::Duration::from_secs(3));
+
+    // Find the first story
+    let first_story = tab.find_element("article.Story")?;
+
+    println!("first story ok");
+
+    // Locate the comments link in the first story
+    let comments_link: headless_chrome::Element<'_> = first_story.find_element("a")?;
+    println!("comments link ok");
+    // Extract the href attribute of the comments link
+    let href = comments_link.get_attribute_value("href")?;
+    println!("href ok");
+    if let Some(link) = href {
+        println!("Comments link: {}", link);
+        tab.navigate_to(&link)?;
+        thread::sleep(time::Duration::from_secs(3));
+        let comment_tree = tab.find_element("table.comment-tree")?;
+        let all_comments_text = comment_tree.get_inner_text()?;
+        println!("{}", all_comments_text);
+    } else {
+        println!("No comments link found.");
     }
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let input = "Elvis Aaron Presley";
-    query(input)
+    query()
 }
