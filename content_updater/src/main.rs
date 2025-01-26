6726a5ea465c6d5ef7a2e983ec25e9523f4e674a
William Droz <william.droz.ch@gmail.com>
2025-01-26 11:11:21 +0100

    feat(content_updater): generate a story with openai from the hn comments

--- content_updater/src/main.rs
+++ content_updater/src/main.rs
@@ -2,10 +2,21 @@ use anyhow::{anyhow, Result};
 use std::{thread, time};
 use headless_chrome::{Browser, LaunchOptions};
 
+use genai::chat::printer::print_chat_stream;
+use genai::chat::{ChatMessage, ChatRequest};
+use genai::Client;
+
+const MODEL: &str = "gpt-4o";
+
 struct HNQueryResult {
     link: String,
     title: String,
     raw_text: String,
+}
+
+struct TypingTextEntry {
+    sources: Vec<String>,
+    story: String,
 }
 
 async fn query() -> Result<HNQueryResult> {
@@ -46,13 +57,44 @@ async fn query() -> Result<HNQueryResult> {
         Err(anyhow!("No comments link found."))
     }
 }
+
+async fn generate_typing_text_entry(hn_query_result: &HNQueryResult) -> Result<TypingTextEntry> {
+    let client = Client::default();
+    let chat_req_str = format!("For a typing training program, you need to create a positive and interesting text based the hacker news article {}. You will need to infer the content of the article from the comments, please answer close to 250 words in lowercase and without ponctuation.", hn_query_result.title);
+	let mut chat_req = ChatRequest::default().with_system(chat_req_str);
+	// This is similar to sending initial system chat messages (which will be cumulative with system chat messages)
+
+    let question = hn_query_result.raw_text.clone();
+
+    chat_req = chat_req.append_message(ChatMessage::user(question));
+
+    println!("\n--- Answer:");
+    let chat_res = client.exec_chat(MODEL, chat_req.clone(), None).await?;
+
+    if let Some(text_result) = chat_res.content_text_as_str() {
+        Ok(TypingTextEntry {
+            sources: vec![hn_query_result.link.clone()],
+            story: text_result.to_string()
+        })
+    }
+    else {
+        Err(anyhow!("Issue with the LLM answers"))
+    }
+
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     if let Ok(hn_qr) = query().await  {
-        println!("{}", hn_qr.raw_text);
         println!("{}", hn_qr.title);
         println!("{}", hn_qr.link);
-        Ok(())
+        if let Ok(story_entry) = generate_typing_text_entry(&hn_qr).await {
+            println!("{}", story_entry.story);
+            Ok(())
+        }
+        else {
+            Err(anyhow!("Unable to generate the story"))
+        }
     }
     else {
         Err(anyhow!("Issue while scraping"))
