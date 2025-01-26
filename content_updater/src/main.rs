6d68c8c2e88610c615accd83ce1fbad1d65b2891
William Droz <william.droz.ch@gmail.com>
2025-01-26 13:46:33 +0100

    chore(content_updater): cargo fmt

--- content_updater/src/main.rs
+++ content_updater/src/main.rs
@@ -1,12 +1,12 @@
 use anyhow::{anyhow, Result};
-use std::{thread, time};
-use headless_chrome::{Browser, LaunchOptions};
-use serde::{Deserialize, Serialize};
+use chrono::{DateTime, Utc};
 use firestore::{FirestoreDb, FirestoreDbOptions, FirestoreQueryDirection, FirestoreResult};
 use genai::chat::{ChatMessage, ChatRequest};
 use genai::Client;
-use chrono::{DateTime, Utc};
+use headless_chrome::{Browser, LaunchOptions};
+use serde::{Deserialize, Serialize};
 use std::env;
+use std::{thread, time};
 
 const MODEL: &str = "gpt-4o";
 
@@ -24,7 +24,7 @@ struct Story {
     when: DateTime<Utc>,
 }
 
-async fn get_db() -> Result<FirestoreDb>{
+async fn get_db() -> Result<FirestoreDb> {
     dotenvy::dotenv().ok();
 
     let project_id = env::var("PROJECT_ID").expect("PROJECT_ID not set");
@@ -53,7 +53,8 @@ async fn get_db() -> Result<FirestoreDb>{
 
 async fn save_story(text_entry: &Story) -> Result<()> {
     if let Ok(db) = get_db().await {
-        let object_returned: Story = db.fluent()
+        let object_returned: Story = db
+            .fluent()
             .insert()
             .into("texts")
             .generate_document_id()
@@ -106,8 +107,8 @@ async fn query() -> Result<HNQueryResult> {
 async fn generate_typing_text_entry(hn_query_result: &HNQueryResult) -> Result<Story> {
     let client = Client::default();
     let chat_req_str = format!("For a typing training program, you need to create a positive and interesting text based the hacker news article {}. You will need to infer the content of the article from the comments, please answer close to 250 words in lowercase and without ponctuation.", hn_query_result.title);
-	let mut chat_req = ChatRequest::default().with_system(chat_req_str);
-	// This is similar to sending initial system chat messages (which will be cumulative with system chat messages)
+    let mut chat_req = ChatRequest::default().with_system(chat_req_str);
+    // This is similar to sending initial system chat messages (which will be cumulative with system chat messages)
 
     let question = hn_query_result.raw_text.clone();
 
@@ -122,16 +123,14 @@ async fn generate_typing_text_entry(hn_query_result: &HNQueryResult) -> Result<S
             story: text_result.to_string(),
             when: Utc::now(),
         })
-    }
-    else {
+    } else {
         Err(anyhow!("Issue with the LLM answers"))
     }
-
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    if let Ok(hn_qr) = query().await  {
+    if let Ok(hn_qr) = query().await {
         println!("{}", hn_qr.title);
         println!("{}", hn_qr.link);
         if let Ok(story_entry) = generate_typing_text_entry(&hn_qr).await {
@@ -140,12 +139,10 @@ async fn main() -> Result<()> {
                 Ok(()) => Ok(()),
                 _ => Err(anyhow!("Unable to save the story to the cloud")),
             }
-        }
-        else {
+        } else {
             Err(anyhow!("Unable to generate the story"))
         }
-    }
-    else {
+    } else {
         Err(anyhow!("Issue while scraping"))
     }
 }
